f1d0637ad0bb721e6b76f1fbfb0525b97fc87987
nofriagara <nofriagara@gmail.com>
2019-04-29 10:54:15 +0700

    single

--- wp-content/themes/partyfairy/woocommerce/content-single-product.php
+++ wp-content/themes/partyfairy/woocommerce/content-single-product.php
@@ -13,6 +13,8 @@
             woocommerce_breadcrumb($args);
 
             $vendor_url = new SimpleXMLElement(do_shortcode('[wcfm_store_info id="" data="store_url"]'));
+
+            print_r( $vendor_url);
             $vendor_gravatar = new SimpleXMLElement(do_shortcode('[wcfm_store_info id="" data="store_gravatar"]'));
 
             ?>
