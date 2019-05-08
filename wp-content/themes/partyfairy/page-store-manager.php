3d9ef05dba3aa3403ae9467d7afa4b22059ec031
nofriagara <nofriagara@gmail.com>
2019-05-08 10:06:51 +0700

    single

--- wp-content/themes/partyfairy/page-store-manager.php
+++ wp-content/themes/partyfairy/page-store-manager.php
@@ -1,17 +1,4 @@
  <?php /* Template Name: Page Store Manager */ ?>
- <?php
- get_header();
- if (have_posts()) : while (have_posts()) : the_post(); ?>
-    <div class="page-content">
-      <section>
-        <div id="checkout" class="container">
-
-<?php the_content() ?>
-</div>
-</section>
-</div>
- 	<?php endwhile; else: ?>
-                <center><article><h3>Sorry, no posts matched your criteria</h3></article></center>
-<?php endif; ?>
-
-<?php get_footer(); ?>
+ <?php wp_head(); ?>
+ <?php echo do_Shortcode('[wc_frontend_manager]') ?>
+ <?php wp_footer() ?>
