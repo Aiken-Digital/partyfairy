899975c59959c8dffdcba9806d2b86a199c178c5
nofriagara <nofriagara@gmail.com>
2019-05-15 12:03:50 +0700

    css

--- wp-content/themes/partyfairy/page-landing.php
+++ wp-content/themes/partyfairy/page-landing.php
@@ -85,7 +85,7 @@ foreach ($f as $key => $value) {
             <div class="container">
                <div class="testimonial-area">
               <div class="row justify-content-center">
-                <div class="col-lg-8">
+                <div class="col-lg-11">
                   <div class="-carousel-testimonial p-t-45">
 
                           <?php $s = get_field('they_say', get_option('page_for_posts')); 
@@ -109,7 +109,7 @@ foreach ($f as $key => $value) {
 
           <div class="container">
             <div class="row justify-content-center">
-              <div class="col-lg-12">
+              <div class="col-lg-11">
                 <div class="hero-banner hero-banner--darker hero-banner--standard -my-paroller" style="background: url(<?php the_field('background_image_gt', get_option('page_for_posts')); ?>) center -68px / cover no-repeat; ">
                   <div class="hero-banner--text">
                     <div class="row">
