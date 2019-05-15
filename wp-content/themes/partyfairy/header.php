d73c979e9adbce466ee322acaf4981d1d5183698
nofriagara <nofriagara@gmail.com>
2019-05-15 21:34:55 +0700

    s

--- wp-content/themes/partyfairy/header.php
+++ wp-content/themes/partyfairy/header.php
@@ -3,8 +3,7 @@
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
- <!--    <title><?php //the_title() ?></title> -->
-    <title>Party Supplies Singapore</title>
+    <title>Party Fairy: Party Supplies Singapore</title>
     <meta name="language" content="en">
     <meta name="apple-mobile-web-app-capable" content="yes">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0">
