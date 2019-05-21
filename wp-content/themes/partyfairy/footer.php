18e6edac6cf645c3f5e16e26d629a4c4bddbcd57
Davit <nofriagara@gmail.com>
2019-05-21 15:37:00 +0700

    s

--- wp-content/themes/partyfairy/footer.php
+++ wp-content/themes/partyfairy/footer.php
@@ -120,8 +120,8 @@
   </div>
 
 </footer>
-
-<div id="sticky"><strong>LOOKING FOR SOMETHING?</strong> <br /> The site is down for planned maintenance but you'll be able to ring/drop her a message at +65 9896 4800, or email us at <a href="mailto:customercare@partyfairy.com">customercare@partyfairy.com</a></div>
+<!-- 
+  <div id="sticky"><strong>LOOKING FOR SOMETHING?</strong> <br /> The site is down for planned maintenance but you'll be able to ring/drop her a message at +65 9896 4800, or email us at <a href="mailto:customercare@partyfairy.com">customercare@partyfairy.com</a></div> -->
 
 <?php wp_footer() ?>
 
