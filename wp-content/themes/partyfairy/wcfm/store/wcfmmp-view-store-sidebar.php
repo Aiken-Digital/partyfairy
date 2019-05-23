8dba3f1296c975287cab35c576e835270423d833
Davit <nofriagara@gmail.com>
2019-05-23 11:04:36 +0700

    s

---
+++ wp-content/themes/partyfairy/wcfm/store/wcfmmp-view-store-sidebar.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * The Template for displaying store sidebar.
+ *
+ * @package WCfM Markeplace Views Store Sidebar
+ *
+ * For edit coping this to yourtheme/wcfm/store 
+ *
+ */
+
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
