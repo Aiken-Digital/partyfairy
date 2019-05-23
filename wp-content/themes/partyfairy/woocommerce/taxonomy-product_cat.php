9c4d036f467aaae8877d6ff3e672794fda86b2a9
Davit <nofriagara@gmail.com>
2019-05-23 12:05:22 +0700

    s

--- wp-content/themes/partyfairy/woocommerce/taxonomy-product_cat.php
+++ wp-content/themes/partyfairy/woocommerce/taxonomy-product_cat.php
@@ -121,7 +121,7 @@ $count_post 	= $the_query->found_posts;
               ?>
 
               <div class="col-lg-3 col-md-6 col-6 tiles-box text-center"><a class="tiles--single" href="<?php the_title() ?>">
-                <div class="tiles--single--img"><img class="img-fluid" src="?php if ( has_post_thumbnail() ) {the_post_thumbnail_url('full'); } else { echo get_template_directory_uri().'/images/broken/img-not-available-landscape.png'; } ?>"></div><a class="tiles--single--model" href=""><?php the_title() ?></a></a>
+                <div class="tiles--single--img"><img class="img-fluid" src="<?php if ( has_post_thumbnail() ) {the_post_thumbnail_url('full'); } else { echo get_template_directory_uri().'/images/broken/img-not-available-landscape.png'; } ?>"></div><a class="tiles--single--model" href=""><?php the_title() ?></a></a>
                 <div class="tiles--price">$130.00<span>each</span></div>
                 <div class="tiles--code">290-000626</div><a class="tiles--seller" href="#"><?php echo $result_name_vendor ?></a><a class="btn btn-rounded btn-hover btn-main btn-solid p-t-10 p-b-10 p-l-20 p-r-20 font-11" href="#">DETAILS</a>
               </div>
