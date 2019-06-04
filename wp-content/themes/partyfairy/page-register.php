778e10af42c81e2abca0b197e81d2327644f19e3
Davit <nofriagara@gmail.com>
2019-06-04 10:07:05 +0700

    s

--- wp-content/themes/partyfairy/page-register.php
+++ wp-content/themes/partyfairy/page-register.php
@@ -23,7 +23,7 @@
  						<div class="divide m-t-30 m-b-25"><span class="divide-text">or</span></div>
  						
  						<div class="social-signin m-t-15 m-b-20">
- 							<a href="https://fixxstaging.com/partyfairy/wp-login.php?loginSocial=facebook&redirect=https://fixxstaging.com/partyfairy/registration/">
+ 							<a href="https://fixxstaging.com/partyfairy/wp-login.php?loginSocial=facebook&redirect=https://fixxstaging.com/partyfairy/my-account/">
  								<span class="btn-social btn-social--fb"><span class="svg-icon m-r-15">﻿<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50" style="fill: rgb(255, 255, 255);">
  									<path d="M40,0H10C4.486,0,0,4.486,0,10v30c0,5.514,4.486,10,10,10h30c5.514,0,10-4.486,10-10V10C50,4.486,45.514,0,40,0z M39,17h-3 c-2.145,0-3,0.504-3,2v3h6l-1,6h-5v20h-7V28h-3v-6h3v-3c0-4.677,1.581-8,7-8c2.902,0,6,1,6,1V17z"/>
  								</svg></span>Sign in with Facebook</span>
@@ -31,7 +31,7 @@
  						</div>
 
  						<div class="social-signin m-t-15">
- 							<a href="https://fixxstaging.com/partyfairy/wp-login.php?loginSocial=google&redirect=https://fixxstaging.com/partyfairy/registration/">
+ 							<a href="https://fixxstaging.com/partyfairy/wp-login.php?loginSocial=google&redirect=https://fixxstaging.com/partyfairy/my-account/">
  								<span class="btn-social btn-social--google"><span class="svg-icon m-r-15">
  									<xml version="1.0" encoding="UTF-8">
  										<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 50 50" version="1.1">
@@ -55,9 +55,9 @@
 
 
  							<div class="divide m-t-30 m-b-25"><span class="divide-text">or</span></div>
- 							
+
  							<div class="social-signin m-t-15 m-b-20">
- 								<a href="https://fixxstaging.com/partyfairy/wp-login.php?loginSocial=facebook&redirect=https://fixxstaging.com/partyfairy/registration/">
+ 								<a href="https://fixxstaging.com/partyfairy/wp-login.php?loginSocial=facebook&redirect=https://fixxstaging.com/partyfairy/my-account/">
  									<span class="btn-social btn-social--fb"><span class="svg-icon m-r-15">﻿<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50" style="fill: rgb(255, 255, 255);">
  										<path d="M40,0H10C4.486,0,0,4.486,0,10v30c0,5.514,4.486,10,10,10h30c5.514,0,10-4.486,10-10V10C50,4.486,45.514,0,40,0z M39,17h-3 c-2.145,0-3,0.504-3,2v3h6l-1,6h-5v20h-7V28h-3v-6h3v-3c0-4.677,1.581-8,7-8c2.902,0,6,1,6,1V17z"/>
  									</svg></span>Sign in with Facebook</span>
@@ -65,7 +65,7 @@
  							</div>
 
  							<div class="social-signin m-t-15">
- 								<a href="https://fixxstaging.com/partyfairy/wp-login.php?loginSocial=google&redirect=https://fixxstaging.com/partyfairy/registration/">
+ 								<a href="https://fixxstaging.com/partyfairy/wp-login.php?loginSocial=google&redirect=https://fixxstaging.com/partyfairy/my-account/">
  									<span class="btn-social btn-social--google"><span class="svg-icon m-r-15">
  										<xml version="1.0" encoding="UTF-8">
  											<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 50 50" version="1.1">
@@ -79,7 +79,7 @@
  									</a>
  								</div>
 
- 								
+
 
 
 
