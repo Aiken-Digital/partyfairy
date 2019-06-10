64ae5e99c7d5104e8790cb831972cc892c63c5f7
Davit <nofriagara@gmail.com>
2019-06-10 10:15:31 +0700

    s

--- wp-content/themes/partyfairy/acf.php
+++ wp-content/themes/partyfairy/acf.php
@@ -1,4 +1,5 @@
 <?php
+
 if( function_exists('acf_add_local_field_group') ):
 
 	acf_add_local_field_group(array(
@@ -886,6 +887,30 @@ acf_add_local_field_group(array(
 					'maxlength' => '',
 				),
 				array(
+					'key' => 'field_5cfdca80edac9',
+					'label' => 'Icon',
+					'name' => 'icon',
+					'type' => 'image',
+					'instructions' => '',
+					'required' => 0,
+					'conditional_logic' => 0,
+					'wrapper' => array(
+						'width' => '',
+						'class' => '',
+						'id' => '',
+					),
+					'return_format' => 'url',
+					'preview_size' => 'thumbnail',
+					'library' => 'all',
+					'min_width' => '',
+					'min_height' => '',
+					'min_size' => '',
+					'max_width' => '',
+					'max_height' => '',
+					'max_size' => '',
+					'mime_types' => '',
+				),
+				array(
 					'key' => 'field_5cd3e57803587',
 					'label' => 'Description',
 					'name' => 'description',
@@ -1723,4 +1748,5 @@ acf_add_local_field_group(array(
 ));
 
 endif;
+
 ?>
