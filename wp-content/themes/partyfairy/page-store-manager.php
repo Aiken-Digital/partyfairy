02adb5718a03e939b7c2c2eb8ce23b776c813a46
Davit <nofriagara@gmail.com>
2019-06-11 14:41:51 +0700

    s

--- wp-content/themes/partyfairy/page-store-manager.php
+++ wp-content/themes/partyfairy/page-store-manager.php
@@ -1,5 +1,4 @@
  <?php /* Template Name: Page Store Manager */ ?>
  <?php get_header(); ?>
- <?php echo do_shortcode('[wc_frontend_manager]') ?>
- <?php //echo do_Shortcode('[dokan-dashboard]') ?>
+ <?php the_content() ?>
  <?php get_footer() ?>
