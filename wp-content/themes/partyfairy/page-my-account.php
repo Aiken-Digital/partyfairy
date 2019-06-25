9fedd617026cb551eeab7b49eebc7bfcf72ceff8
nofriagara <nofriagara@gmail.com>
2019-06-26 00:00:28 +0700

    core

--- wp-content/themes/partyfairy/page-my-account.php
+++ wp-content/themes/partyfairy/page-my-account.php
@@ -1,5 +1,8 @@
  <?php /* Template Name: Page My Account*/ ?>
- <?php get_header() ?>
+ <?php get_header();
+ if (have_posts()) : while (have_posts()) : the_post(); ?>
+ ?>
+
   <div class="page-content">
       <section class="m-b-60">
       	<div class="container">
@@ -7,6 +10,7 @@
 	    </div>
 		</section>
 		</div>
+		
 <?php endwhile; else: ?>
                 <center><article><h3>Sorry, no posts matched your criteria</h3></article></center>
 <?php endif; ?>
