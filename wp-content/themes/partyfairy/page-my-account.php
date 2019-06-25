6da7a04cfcc5e71f13f1e5d12b354bc664d3fb72
nofriagara <nofriagara@gmail.com>
2019-06-25 23:59:05 +0700

    core

--- wp-content/themes/partyfairy/page-my-account.php
+++ wp-content/themes/partyfairy/page-my-account.php
@@ -1,3 +1,14 @@
  <?php /* Template Name: Page My Account*/ ?>
  <?php get_header() ?>
+  <div class="page-content">
+      <section class="m-b-60">
+      	<div class="container">
+		<?php the_content() ?>
+	    </div>
+		</section>
+		</div>
+<?php endwhile; else: ?>
+                <center><article><h3>Sorry, no posts matched your criteria</h3></article></center>
+<?php endif; ?>
+
  <?php get_footer() ?>
